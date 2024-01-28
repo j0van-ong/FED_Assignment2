3d36d4ee7177ebacb9d760c50563f3aabde043d9
j0van-ong <s10258240@connect.np.edu.sg>
2024-01-29 00:07:33 +0800

    Layout of ingame page done

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 })
 
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
